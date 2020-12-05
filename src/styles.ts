493c8f5441ca41394362c74614bc5c49d7351c15
drzamich <drzamich@gmail.com>
2020-12-05 19:22:05 +0100

    feat: remove unnecessary focus outline

--- src/styles.ts
+++ src/styles.ts
@@ -17,6 +17,10 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
     font-family: 'Roboto', sans-serif;
+
+    &:focus:not(:focus-visible) {
+      outline: none;
+    }
   }
   html, body, #root {
     height: 100%;

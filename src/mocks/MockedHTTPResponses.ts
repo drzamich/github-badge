e4a5bce5265cb3613f1a68c27a3c9c399e91ae6c
drzamich <drzamich@gmail.com>
2020-12-05 17:12:29 +0100

    feat: add tests for api handler

---
+++ src/mocks/MockedHTTPResponses.ts
@@ -0,0 +1,60 @@
+import { AxiosError, AxiosResponse } from 'axios';
+import UserEndpointResponse from './UserEndpointResponse.json';
+import ReposEndpointResponse from './ReposEndpointResponse.json';
+import { Repository, User } from '../interfaces';
+
+const defResponse = {
+  headers: [],
+  config: {},
+};
+
+const defSuccessResponse = {
+  ...defResponse,
+  status: 200,
+  statusText: 'success',
+};
+
+const defErrorResponse: AxiosResponse = {
+  ...defResponse,
+  status: 500,
+  statusText: 'success',
+  data: null,
+};
+
+const defError: AxiosError = {
+  config: {},
+  isAxiosError: true,
+  toJSON: () => ({}),
+  name: '',
+  message: '',
+};
+
+export const userReponse: AxiosResponse<User> = {
+  data: UserEndpointResponse,
+  ...defSuccessResponse,
+};
+
+export const reposResponse: AxiosResponse<Repository[]> = {
+  data: ReposEndpointResponse,
+  ...defSuccessResponse,
+};
+
+export const networkError: AxiosError = {
+  ...defError,
+  request: {},
+};
+
+export const internalServerError: AxiosError = {
+  ...defError,
+  response: {
+    ...defErrorResponse,
+  },
+};
+
+export const notFoundError: AxiosError = {
+  ...defError,
+  response: {
+    ...defErrorResponse,
+    status: 404,
+  },
+};
---
+++ src/mocks/ParsedUserInfo.ts
@@ -0,0 +1,19 @@
+import { User, Repository } from '../interfaces';
+
+export const parsedUser: User = {
+  name: 'Tejas Kumar',
+  login: 'TejasQ',
+  avatar_url: 'https://avatars1.githubusercontent.com/u/9947422?v=4',
+  bio: 'I write code and love life.',
+};
+
+export const parsedRepos: Repository[] = [
+  { full_name: 'TejasQ/add-gitignore', html_url: 'https://github.com/TejasQ/add-gitignore', stargazers_count: 450 },
+  { full_name: 'TejasQ/basically-fullstack-graphql', html_url: 'https://github.com/TejasQ/basically-fullstack-graphql', stargazers_count: 194 },
+  { full_name: 'TejasQ/basically-streams', html_url: 'https://github.com/TejasQ/basically-streams', stargazers_count: 122 },
+];
+
+export const finalUser: User = {
+  ...parsedUser,
+  repos: parsedRepos,
+};

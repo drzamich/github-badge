c66481e2c965f4438930b8181a581f7628fffd28
drzamich <drzamich@gmail.com>
2020-12-05 14:10:29 +0100

    feat: add tets for Search

---
+++ src/__tests__/Search.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { Search } from '../Search';
+
+describe('Search', () => {
+  it('calls onSearch with proper value when button is clicked', () => {
+    // Given
+    const onSearch = jest.fn();
+    render(<Search onSearch={onSearch} />);
+    const input = screen.getByPlaceholderText('Search for users');
+    const button = screen.getByText('Search');
+    const value = 'foo';
+
+    // When
+    fireEvent.change(input, { target: { value } });
+    fireEvent.click(button);
+
+    // Then
+    expect(onSearch).toBeCalledTimes(1);
+    expect(onSearch).toBeCalledWith(value);
+  });
+});

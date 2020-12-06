a7cc364faf6b5d2d07637f24018805689646e0d1
drzamich <drzamich@gmail.com>
2020-12-06 11:19:41 +0100

    feat: add snapshot test for App

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from '../App';
 
-test('renders without crashing', () => {
-  render(<App />);
+test('renders the app without crashing and matches snapshot', () => {
+  const app = render(<App />);
+  expect(app).toMatchSnapshot();
 });

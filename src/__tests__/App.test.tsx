fc41fc55e61290dc5aec400dc32ad78c7e661bd7
drzamich <drzamich@gmail.com>
2020-12-06 14:53:37 +0000

    Deploying to gh-pages from  @ 89d8bd802ca8b8d9b5e9292e791ad5954e6b8886 🚀

--- src/__tests__/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from '../App';
-
-test('renders the app without crashing and matches snapshot', () => {
-  const app = render(<App />);
-  expect(app).toMatchSnapshot();
-});
